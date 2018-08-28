99d6b4fc52f2b4de250894168395a224a21eb93b
tanzanlong <15218835680@163.com>
2018-08-28 17:56:16 +0800

    blockchain

---
+++ blockchain-one-service/src/main/scripts/fat2/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#变量设置
+SERVICE_NAME=acct-customer-service
+PROJECT_DIR=$(cd "$(dirname "$0")"; pwd)
+PROJECT_DIR=$PROJECT_DIR/..
+CONFIG_DIR=$PROJECT_DIR/config
+LIB_DIR=$PROJECT_DIR/lib
+LIB_JARS=`ls $LIB_DIR | grep .jar | awk '{print "'$LIB_DIR'/"$0}' | tr "\n" ":"`
+mkdir -p /logs/$SERVICE_NAME/
+MAIN_CLASS="com.baibei.settlement.customer.Bootstrap"
+# 设置classpath
+#nohup java -classpath $CONFIG_DIR:$LIB_JARS $MAIN_CLASS &
+#由于使用docker，所以无法使用nohup  将进程挂起，也不能使用重定向等
+#java -Dspring.profiles.active=uat -classpath $CONFIG_DIR:$LIB_JARS $MAIN_CLASS
+
+java -Xms1024M -Xmx2048M -Xmn1536M -Xss512k -Denv=fat -Dfat_meta=http://192.168.12.150:1666 -Dapollo.cluster=fat2 -classpath $CONFIG_DIR:$LIB_JARS $MAIN_CLASS | tee -a /logs/$SERVICE_NAME/std_out.log
---
+++ blockchain-one-service/src/main/scripts/prod/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#变量设置
+PROJECT_NAME=Q
+SERVICE_NAME=acct-customer-service
+PROJECT_DIR=$(cd "$(dirname "$0")"; pwd)
+PROJECT_DIR=$PROJECT_DIR/..
+CONFIG_DIR=$PROJECT_DIR/config
+LIB_DIR=$PROJECT_DIR/lib
+LIB_JARS=`ls $LIB_DIR | grep .jar | awk '{print "'$LIB_DIR'/"$0}' | tr "\n" ":"`
+mkdir -p /logs/$SERVICE_NAME/
+MAIN_CLASS="com.baibei.settlement.customer.Bootstrap"
+# 设置classpath
+#nohup java -classpath $CONFIG_DIR:$LIB_JARS $MAIN_CLASS &
+#由于使用docker，所以无法使用nohup  将进程挂起，也不能使用重定向等
+#java -Dspring.profiles.active=press -classpath $CONFIG_DIR:$LIB_JARS $MAIN_CLASS
+
+java -Xms1024M -Xmx2048M -Xmn1536M -Xss512k -Dapp.id=${PROJECT_NAME}"_"${SERVICE_NAME} -Denv=pro -Dpro_meta=http://192.168.22.143:80 -classpath $CONFIG_DIR:$LIB_JARS $MAIN_CLASS | tee -a /logs/$SERVICE_NAME/std_out.log
